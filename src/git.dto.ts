48406417ce4914a84a3531f3531a46f4e5d703c7
Murilo Varela <sergiomurilovarela@gmail.com>
2022-02-12 13:25:41 -0300

    Adds git services

---
+++ src/git.dto.ts
@@ -0,0 +1,110 @@
+export type GitlabUser = {
+  id: number;
+  name: string;
+  username: string;
+  avatar_url: string;
+  email?: string;
+  state?: string;
+  web_url?: string;
+};
+
+export type GitlabProject = {
+  id: number;
+  name: string;
+  description: string;
+  web_url: string;
+  avatar_url: string | null;
+  git_ssh_url: string;
+  git_http_url: string;
+  namespace: string;
+  visibility_level: number;
+  path_with_namespace: string;
+  default_branch: string;
+  ci_config_path: string;
+  homepage: string;
+  url: string;
+  ssh_url: string;
+  http_url: string;
+};
+
+export type ObjectAttributes = {
+  assignee_id: number;
+  author_id: number;
+  created_at: string;
+  description: string;
+  head_pipeline_id: string | null;
+  id: number;
+  iid: number;
+  last_edited_at: string | null;
+  last_edited_by_id: string | null;
+  merge_commit_sha: string | null;
+  merge_error: string | null;
+  merge_params: unknown;
+  merge_status: string;
+  merge_user_id: number | null;
+  merge_when_pipeline_succeeds: boolean;
+  milestone_id: number | null;
+  source_branch: string;
+  source_project_id: number;
+  state_id: number;
+  target_branch: string;
+  target_project_id: number;
+  time_estimate: number;
+  title: string;
+  updated_at: string;
+  updated_by_id: number | null;
+  url: string;
+  source: unknown;
+  target: unknown;
+  last_commit: unknown;
+  work_in_progress: boolean;
+  total_time_spent: number;
+  time_change: number;
+  human_total_time_spent: number | null;
+  human_time_change: number | null;
+  human_time_estimate: number | null;
+  assignee_ids: Array<number>;
+  state: string;
+  blocking_discussions_resolved: boolean;
+};
+
+export type GitlabRepository = {
+  name: string;
+  url: string;
+  description: string;
+  homepage: string;
+};
+
+export type GitlabMergeRequestEventDto = {
+  object_kind: string;
+  event_type: string;
+  user: GitlabUser;
+  project: GitlabProject;
+  object_attributes: ObjectAttributes;
+  labels: Array<unknown>;
+  changes: unknown;
+  repository: GitlabRepository;
+  assignees: Array<GitlabUser>;
+};
+
+export type GitlabGetMergeRequestDto = {
+  webUrl: string;
+};
+
+export type GitlabGetMergeRequestApprovalsDto = {
+  projectId: string | number;
+  mergeRequestIid: number;
+};
+
+export type MergeRequestResponseDto = Record<string, unknown> & {
+  id: number;
+  iid: number;
+  project_id: number;
+  title: string;
+  description: string;
+  state: string;
+  reviewers: GitlabUser[];
+  draft: boolean;
+  merge_status: string;
+  approved_by: { user: GitlabUser }[];
+};
---
+++ src/git.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { Client as Gitlab } from '@nerdvision/gitlab-js';
+import { GitlabGetMergeRequestDto, MergeRequestResponseDto } from './git.dto';
+import { getIdsFromUrl } from './helpers';
+
+@Injectable()
+export class GitService {
+  async getMergeRequest({
+    webUrl,
+  }: GitlabGetMergeRequestDto): Promise<MergeRequestResponseDto> {
+    const gitlabClient = new Gitlab({
+      token: process.env.GITLAB_APP_OAUTH,
+    });
+    const { projectId, mergeRequestIid } = getIdsFromUrl(webUrl);
+
+    if (!projectId || !mergeRequestIid) return null;
+
+    const project = await gitlabClient.projects.find(projectId);
+
+    if (!project) return null;
+
+    const mergeRequest = await project.mergeRequests.find(
+      parseInt(mergeRequestIid),
+    );
+
+    if (!mergeRequest) return null;
+
+    const approvals = await mergeRequest.approvals();
+
+    if (!approvals) return null;
+
+    return { ...mergeRequest.data, approved_by: approvals.data.approved_by };
+  }
+}
