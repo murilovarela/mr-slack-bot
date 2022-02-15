9cbd8c63f7bd88457695b9ab5afc1edf65b6fb2d
Murilo Varela <sergiomurilovarela@gmail.com>
2022-02-15 13:25:19 -0300

    Setup for heroku deploy

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
